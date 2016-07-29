873a180682ae4683a82f9a9f355d97d21a86852c
Amanguchi <johncorderox@gmail.com>
2016-07-29 03:05:11 -0700

    New javascript files and more bootstrap for the input field

---
+++ js/animations.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $('#inputexample').hide();
+  $('.final').hide();
+
+
+  $('#inputbutton').click(function(){
+
+      $('#inputexample').toggle("fast");
+
+
+  });
+
+
+});
---
+++ js/functions.js
@@ -0,0 +1,5 @@
+// Main functions for the form builder web application.
+//
+// @Author John Cordero
+
+var output = "";
